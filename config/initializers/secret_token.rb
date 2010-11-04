5bfa937987c590a41e16298bd929799be7dee1d5
Stefan Pettersson <stefan.pettersson@lumano.se>
2010-11-04 10:59:26 +0100

    First commit, just created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cal3::Application.config.secret_token = 'ed39018d0972ac01fb58c76b3aeeaf68b8a3fd6bf172373a94ce003b7b6ea9d2ff6272f43b161f6ef4aad13940e018eb58b97c4d40cca73b04972b08a7cb140c'
