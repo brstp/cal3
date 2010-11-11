83335b2f5273049f66d711971785bc205388453b
Stefan Pettersson <stefan.pettersson@lumano.se>
2010-11-11 13:46:35 +0100

    Removed bailing code (virtual date/time attributes)

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Cal3
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Stockholm'
+    # config.time_zone = 'Stockholm'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
