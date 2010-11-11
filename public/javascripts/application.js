f64d0804fc66cdaf86cc39d3c1c5b46956dbe791
Stefan Pettersson <stefan.pettersson@lumano.se>
2010-11-11 23:12:59 +0100

    Added javascript date picker

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function() {
+$.datepicker.setDefaults({
+   maxDate: '+2y',
+   showWeek: true,
+//   showOn: 'both',
+//   buttonImageOnly: false,
+ //  buttonImage: 'calendar.gif',
+//   buttonText: 'Calendar' 
+});
+
+$("#event_start_date").datepicker();
+});
