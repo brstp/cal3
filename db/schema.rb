f170780ec8390796f9911747a5939b9ff8c7d7c2
Stefan Pettersson <stefan.pettersson@lumano.se>
2010-11-17 16:56:03 +0100

    ran rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(:version => 20101117101548) do
     t.datetime "updated_at"
   end
 
+  create_table "organizers", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "website"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
     t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
@@ -54,7 +62,5 @@ ActiveRecord::Schema.define(:version => 20101117101548) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
-end
 
 end
