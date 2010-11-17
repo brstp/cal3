c17c901a10a9ac21436b6f0d885019e737b58d18
Stefan Pettersson <stefan.pettersson@lumano.se>
2010-11-17 15:31:33 +0100

    Links from organizer and municipality to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,6 +14,8 @@ class Event < ActiveRecord::Base
   validates_numericality_of :lng, :allow_nil => true
   validate :validates_start_time, :validates_start_date, :validates_stop_time, :validates_stop_date, :validates_start_stop
   
+
+  
   def duration
     duration = start_datetime.strftime('%A %d %B %Y kl. %H:%M') + " - " 
     if @start_date != @stop_date
--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -2,4 +2,10 @@ class Organizer < ActiveRecord::Base
   has_many :events
   attr_accessible :name, :description, :website
   validates_presence_of :name, :description
+  validates_length_of :name, :in => 8..40
+  
+  
+  def my_events
+    Event.where(:organizer_id => @id).all
+  end
 end
