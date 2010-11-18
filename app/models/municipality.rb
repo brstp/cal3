e83e37f80a791520169dfd8d34cc37a96626b682
Stefan Pettersson <stefan.pettersson@lumano.se>
2010-11-18 13:24:16 +0100

    .

--- app/models/municipality.rb
+++ app/models/municipality.rb
@@ -1,7 +1,7 @@
 class Municipality < ActiveRecord::Base
   has_many :events
-  attr_accessible :name, :short_name, :admin_id, :parent_admin_id
-  validates_presence_of  :name, :short_name, :admin_id, :parent_admin_id
+  attr_accessible :name, :short_name, :admin_no, :parent_admin_no
+  validates_presence_of  :name, :short_name, :admin_no, :parent_admin_no
   validates_length_of :name, :in => 5..40
-  validates_uniqueness_of :name, :short_name, :admin_id
+  validates_uniqueness_of :name, :short_name, :admin_no
 end
