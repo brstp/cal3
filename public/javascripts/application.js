52fb5f8a1b150943964fc4f3fb5bf37fa1536c27
Micael Larsson <micael.bc.larsson@gmail.com>
2010-11-25 10:59:25 +0100

    Added set focus method

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,3 +14,17 @@ $.datepicker.setDefaults({
  $("#event_start_date").datepicker();
  $("#event_stop_date").datepicker();
 });
+
+// Sets the form focus to the first element found in forms[0] that
+// is a textfield or text area
+$(document).ready(function(){
+  if (document.forms[0] == null) return;
+
+  for (var i = 0; i < document.forms[0].elements.length; i++) {
+    e = document.forms[0].elements[i];
+    if ((e.type == "text") || (e.type == "textarea")) {
+      e.focus();
+      break;
+    }
+  }
+});
