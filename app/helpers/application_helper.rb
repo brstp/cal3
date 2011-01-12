8dd9bb44669cf406ea349c0ec72f4432a5d42723
Stefan Pettersson <stefan.pettersson@lumano.se>
2011-01-12 10:16:53 +0100

    Gravatar #62

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def avatar_url(user)  
+    default_url = "#{root_url}images/guest.png"  
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase  
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"   
+  end
+  
+  
 end
