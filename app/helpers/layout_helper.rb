ee49b4622e0fbd852def588b1847a6e788e17203
Stefan Pettersson <stefan.pettersson@lumano.se>
2011-01-26 15:25:16 +0100

    ongoing work with calendar table. not finished

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # These helper methods can be called in your template to set variables to be used in the layout
 # This module should be included in all views globally,
 # to do so you may need to add this line to your ApplicationController
@@ -44,4 +45,76 @@ module LayoutHelper
     raw (t('app.in_municipality') + ' ' + link_to( (event.municipality.name + ' (' + event.municipality.number_of_upcoming_events.to_s + ')'), event.municipality ))
   end
   
-end
+  
+  def calendar
+    #TODO period in initialize
+    #TODO limit in initializer
+    cal = ""
+    
+    events = Event.where("stop_datetime >= ? AND start_datetime <= ?", 
+                Time.now.beginning_of_day, Time.now.end_of_day + 2.months ).
+                order('start_datetime ASC').limit 200 
+         
+    cal << %(
+          <table class="calendar">
+            <caption>Evenemangskalender för #{l(events.first.start_datetime, :format => :month_and_year)}</caption>
+            )
+    current_month = events.first.start_datetime.beginning_of_month
+    current_day = events.first.start_datetime.beginning_of_day - 1.day
+    
+    for event in events
+      cal << %(<!--  debug -->)
+      logger.info "%%%%%%%%%%%%%%%%%%%%%%%%current_day" + current_day.to_s
+      logger.info "%%%%%%%%%%%%%%%%%%%%%%%%event" + event.start_datetime.beginning_of_day.to_s
+      
+      if event.start_datetime.beginning_of_month > current_month
+        cal << %(</table>)
+        cal << %(
+              <table class="calendar">
+                <caption>Evenemangskalender för #{l(event.start_datetime, :format => :month_and_year)}</caption>
+                )   
+                
+        current_month = event.start_datetime.beginning_of_month
+      end
+      
+      if event.start_datetime.beginning_of_day > current_day
+      cal <<  %(
+              <tr>
+                <th class="day_box">
+                  <span class="day_of_month">
+                    <span class="#{l(event.start_datetime, :format => :day_of_week).gsub("ö", "o").gsub("å", "a")}">
+                      <abbr class="day" title="#{l(event.start_datetime, :format => :date) }">
+                        #{ l(event.start_datetime, :format => :day_of_month) }
+                      </abbr>
+                    </span>
+                  </span>
+                  <br />
+                  <span class="day_of_week">
+                   #{l(event.start_datetime, :format => :day_of_week)}
+                  </span>
+                </th>
+                <td>
+                  <ol class="events_with_time">
+                )
+      end
+
+      cal << %(
+              <li class="event">
+              #{ link_to raw("<strong>" + event.subject + '</strong> ' + content_tag(:abbr,  l(event.start_datetime, :format => :clock), :title => "#{l(event.start_datetime, :format => :machine)}") + ', ' + event.municipality_short) , event, :title => t('app.organizer') + ": " + event.organizer.name + ". " + t('app.summary') +": " + event.intro }
+              </li>)
+      
+      if event.start_datetime.beginning_of_day > current_day # and not another on same day...
+        cal << %(</ol>
+            </td>
+          </tr>)
+        current_day = event.start_datetime.beginning_of_day
+      end  
+     
+    
+    end 
+    cal << %(</table>)
+
+    raw cal
+  end #def calendar
+  
+end # module LatoutHelper
