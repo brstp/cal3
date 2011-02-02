7e98846b636478e8eeefd3e46e35846406913d2e
Stefan Pettersson <stefan.pettersson@lumano.se>
2011-02-02 14:21:22 +0100

    troubleshooting settings for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Cal3::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
