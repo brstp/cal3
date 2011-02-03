653a39481f601659cb24768b09895a066c1ad3fa
Stefan Pettersson <stefan.pettersson@lumano.se>
2011-02-03 12:53:39 +0100

    refactoring of maps

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -21,19 +21,93 @@ module LayoutHelper
     content_for(:head) { javascript_include_tag(*args) }
   end
   
-  def map_marker(lat, lng)
-   str = raw '<script type="text/javascript">'
-   str += raw "$(document).ready(function() { $('#map_canvas').googleMaps({ latitude: #{lat}, longitude: #{lng}, depth: 10, markers: { latitude: #{lat}, longitude: #{lng}, draggable:false } }); });"
-   str += raw '</script>' 
-   str
+  def map_marker(event)
+   str = %(
+        <script type="text/javascript">
+
+        function initialize() {
+          var latLng = new google.maps.LatLng( #{event.lat}, #{event.lng});
+          var map = new google.maps.Map(document.getElementById('mapCanvas'), {
+            zoom: 10,
+            center: latLng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          });
+          var marker = new google.maps.Marker({
+            position: latLng,
+            title: '#{event.subject}  #{event.duration.capitalize}',
+            map: map,
+            draggable: false
+          });
+          
+          updateMarkerPosition(latLng);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+</script>)
+    raw str
   end
 
-#TODO remove or rewrite. Not used  
- def map_marker_draggable(lat, lng)
-   str = raw '<script type="text/javascript">'
-   str += raw "$(document).ready(function() { $('#map_canvas').googleMaps({ latitude: #{lat}, longitude: #{lng}, depth: 10, markers: { latitude: #{lat}, longitude: #{lng}, draggable: true } }); });"
-   str += raw '</script>' 
-   str
+ def draggable_map_marker(event)
+   str = %(
+          <script type="text/javascript">
+          var geocoder = new google.maps.Geocoder();
+
+          function geocodePosition(pos) {
+            geocoder.geocode({
+              latLng: pos
+            }, function(responses) {
+              if (responses && responses.length > 0) {
+                updateMarkerAddress(responses[0].formatted_address);
+              } else {
+                updateMarkerAddress('Cannot determine address at this location.');
+              }
+            });
+          }
+
+
+
+          function updateMarkerPosition(latLng) {
+            document.getElementById('event_lat').value = latLng.lat();
+            document.getElementById('event_lng').value = latLng.lng();
+          }
+
+          function updateMarkerAddress(str) {
+            document.getElementById('address').innerHTML = str;
+          }
+
+          function initialize() {
+            var latLng = new google.maps.LatLng( #{event.lat}, #{event.lng});
+            var map = new google.maps.Map(document.getElementById('mapCanvas'), {
+              zoom: 10,
+              center: latLng,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+            var marker = new google.maps.Marker({
+              position: latLng,
+              title: '#{t('map.drag_me')}',
+              map: map,
+              draggable: true
+            });
+            
+            // Update current position info.
+            updateMarkerPosition(latLng);
+            geocodePosition(latLng);
+            
+            
+            google.maps.event.addListener(marker, 'drag', function() {
+              updateMarkerPosition(marker.getPosition());
+            });
+            
+            google.maps.event.addListener(marker, 'dragend', function() {
+              geocodePosition(marker.getPosition());
+            });
+          }
+
+          // Onload handler to fire off the app.
+          google.maps.event.addDomListener(window, 'load', initialize);
+          </script>
+             )
+   raw str
   end
   
   
