c68899ae254c909bf14b7387d6e02470ed497dd6
Stefan Pettersson <stefan.pettersson@lumano.se>
2011-03-15 12:22:37 +0100

    trying to set postgresql collation

--- config/environment.rb
+++ config/environment.rb
@@ -7,5 +7,6 @@ require File.expand_path('../application', __FILE__)
 #require 'openssl'
 #OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
+
 # Initialize the rails application
 Cal3::Application.initialize!
