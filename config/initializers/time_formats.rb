2791a489897d663223ae43ca7ad48a51af831bb5
Stefan Pettersson <stefan.pettersson@lumano.se>
2011-05-24 13:33:16 +0200

    fixed parsing of time

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -5,6 +5,7 @@ Timeliness.use_euro_formats
 Timeliness.add_formats(:time, 'hh.nn', :before => 'hh:nn:ss')
 Timeliness.add_formats(:time, 'hh.n', :after => 'hh:nn:ss')
 Timeliness.add_formats(:time, 'h.n', :after => 'hh:nn:ss')
+Timeliness.add_formats(:time, 'h.nn', :after => 'hh:nn:ss')
 
 Timeliness.add_formats(:datetime, 'yyyy-mm-dd hh.nn', :before => 'yyyy-mm-dd hh:nn:ss')
 Timeliness.add_formats(:datetime, 'yyyy-mm-dd h.nn', :before => 'yyyy-mm-dd hh:nn:ss')
