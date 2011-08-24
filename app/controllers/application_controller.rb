f5eada9b5b1e849787c1067e882538d5c226b7fd
Stefan Pettersson <stefan.pettersson@lumano.se>
2011-08-24 17:06:09 +0200

    rewrite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
 
   def check_uri
-    redirect_to request.protocol + "www." + request.host_with_port.gsub("/www\.//g") + request.request_uri if /^www/.match(request.host)
+    redirect_to request.protocol + request.host_with_port.gsub("/www\.//g") + request.request_uri if /^www/.match(request.host)
   end
 
 
