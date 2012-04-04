442462a5f06b8494e63e005347d4b0898c7fd225
Stefan Pettersson <stefan.pettersson@lumano.se>
2012-04-04 12:19:58 +0200

    fix to make migrations working from scratch

--- db/migrate/20120207163922_remove_remember_token_from_users.rb
+++ db/migrate/20120207163922_remove_remember_token_from_users.rb
@@ -1,9 +1,9 @@
 class RemoveRememberTokenFromUsers < ActiveRecord::Migration
   def self.up
-    remove_column :users, :remember_token
+#    remove_column :users, :remember_token
   end
 
   def self.down
-    add_column :users, :remember_token, :string
+#    add_column :users, :remember_token, :string
   end
 end
