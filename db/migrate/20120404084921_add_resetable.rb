6410fe42e9377e9a25f19e48e67b356d93961ee4
Stefan Pettersson <stefan.pettersson@lumano.se>
2012-04-04 14:36:16 +0200

    conditional migration depending on how devise was originally built

--- db/migrate/20120404084921_add_resetable.rb
+++ db/migrate/20120404084921_add_resetable.rb
@@ -1,9 +1,9 @@
 class AddResetable < ActiveRecord::Migration
   def up
-      #add_column :users, :reset_password_sent_at, :datetime
+      add_column(:users, :reset_password_sent_at, :datetime) unless User.column_names.include?('reset_password_sent_at')
   end
 
   def down
-      #remove_column :users, :reset_password_sent_at
+      remove_column( :users, :reset_password_sent_at) if User.column_names.include?('reset_password_sent_at')
   end
 end
