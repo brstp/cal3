a71101852b8745975911471351a0fd51c71c0329
Stefan Pettersson <stefan.pettersson@lumano.se>
2012-10-08 18:51:55 +0200

    troubleshooting sql

--- app/models/almanac_day.rb
+++ app/models/almanac_day.rb
@@ -9,7 +9,7 @@ class AlmanacDay < ActiveRecord::Base
   end
   
   def AlmanacDay.names day
-    AlmanacDay.where("day == ? AND month == ?", I18n.l(Time.zone.now, :format => :day_of_month).to_i, I18n.l(Time.zone.now, :format => :month_of_year).to_i).flatten.join(" ")
+    AlmanacDay.where("day = ? AND month = ?", I18n.l(Time.zone.now, :format => :day_of_month).to_i, I18n.l(Time.zone.now, :format => :month_of_year).to_i).flatten.join(" ")
   end
     
 end
