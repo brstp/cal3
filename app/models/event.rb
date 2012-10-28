b9921b0e273326a882552077ceca146b9351ada6
Stefan Pettersson <stefan.pettersson@lumano.se>
2012-10-28 09:53:02 +0100

    quick fix, USING mass assignment temporarily

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,7 +20,7 @@ class Event < ActiveRecord::Base
                   :start_date, :start_time, :stop_date, :stop_time, :phone_number, 
                   :phone_name, :email, :human_name, :category_id, :counter, :start_datetime, 
                   :stop_datetime, :image1, :image2, :image3, 
-                  :image1_caption, :image1_url, :image1_delete, :image2_caption, :image2_url, :image2_delete, :image3_caption, :image3_url, :image3_delete, :init_lat, :init_lng, :init_zoom
+                  :image1_caption, :image1_url, :image1_delete, :image2_caption, :image2_url, :image2_delete, :image3_caption, :image3_url, :image3_delete, :init_lat, :init_lng, :init_zoom, :organizer_id
 
 
   validates_presence_of :subject, :description, :municipality_id, :start_date, :start_time, :organizer_id, :email, :human_name, :category 
