38d6f119f2be1c90ab6e0e75a3cafa64134e2117
Stefan Pettersson <stefan.pettersson@lumano.se>
2012-11-27 14:24:47 +0100

    changed migration for devise to be compatible with current version of devise

--- db/migrate/20120207163922_remove_remember_token_from_users.rb
+++ db/migrate/20120207163922_remove_remember_token_from_users.rb
@@ -1,9 +1,9 @@
 class RemoveRememberTokenFromUsers < ActiveRecord::Migration
   def self.up
-    remove_column( :users, :remember_token)  unless User.column_names.include?('remember_token')
+    #remove_column( :users, :remember_token)  unless User.column_names.include?('remember_token')
   end
 
   def self.down
-    add_column( :users, :remember_token, :string) if User.column_names.include?('remember_token')
+    #add_column( :users, :remember_token, :string) if User.column_names.include?('remember_token')
   end
 end
