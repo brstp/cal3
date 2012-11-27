63b69be569c49da8af9bdbffbf016b5c53c5a80f
Stefan Pettersson <stefan.pettersson@lumano.se>
2012-11-27 14:27:17 +0100

    changed migration for devise to be compatible with current version of devise

--- db/migrate/20120917143313_change_invitor_to_invited_by_id_in_users.rb
+++ db/migrate/20120917143313_change_invitor_to_invited_by_id_in_users.rb
@@ -1,9 +1,9 @@
 class ChangeInvitorToInvitedByIdInUsers < ActiveRecord::Migration
   def up
-    rename_column :users, :invitor, :invited_by_id
+    #rename_column :users, :invitor, :invited_by_id
   end
 
   def down
-    rename_column :users, :invited_by_id, :invitor
+    #rename_column :users, :invited_by_id, :invitor
   end
 end
