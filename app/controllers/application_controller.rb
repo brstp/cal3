7ed24318c4aba8da4879927814199cf6a1d6d83a
Stefan Pettersson <stefan.pettersson@lumano.se>
2012-11-27 09:37:12 +0100

    demo mode with env ALLOM_DEMO

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
+# encoding: UTF-8
+
 require "application_responder"
 
 class ApplicationController < ActionController::Base
   before_filter :check_uri
   before_filter http_basic_authenticate_with(:name => "smygtitt", :password => "tyst" ) if (ENV['ALLOM_LIVE']).blank?
+  
+  before_filter :demo_mode?, :except => [:show, :index]
+
   protect_from_forgery
 
 
@@ -34,6 +39,14 @@ class ApplicationController < ActionController::Base
 
   def remove_admin_params
     params[:user].delete(:is_admin) unless current_user.try(:is_admin)
+  end
+
+
+  def demo_mode?
+    unless (ENV['ALLOM_DEMO']).blank?
+      flash[:alert] = "Hej! Det här är en smygtitt av nästa version av Allom. Här går det inte att spara, radera eller ändra något. Gå till \"vanliga\" allom.se om du vill lägga in eller ändra något. Allt som ligger där kommer att flyttas med när vi kör igång den nya versionen."
+        redirect_to :back
+    end
   end
 
 end
